38c4bed9355a38c915fc1321d86251b868305496
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-03-23 14:08:33 -0400

    Added constructors and for loops

---
+++ 0.08_Loops_ForLoops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_Loops_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <3; i++)
+            {
+                Console.WriteLine("This is a Loop!");
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(i+1);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine("Even!");
+                }
+                else
+                    Console.WriteLine(10-i);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 0.09_Loops_While/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._09_Loops_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
